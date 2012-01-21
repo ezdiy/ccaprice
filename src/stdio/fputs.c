e012e65c02f69554fd3261c569fee008c5afc69b
Dale Weiler <killfieldengine@gmail.com>
2012-01-21 18:55:57 -0500

    Buffered puts(), and fputs().

--- src/stdio/fputs.c
+++ src/stdio/fputs.c
@@ -2,6 +2,15 @@
 #include "inc/string.h"
 int fputs(const char *str, FILE *fp) {
 	if (!fp) return EOF;
-	write(fp->fd, str, strlen(str));
+	
+	/* 
+	 * The function begins copying from the address specified (str) 
+	 * until it reaches the terminating null character ('\0'). 
+	 * This final null-character is not copied to the stream.
+	 */
+	while (*str != '\0') {
+		fp->buffer_dat[fp->buffer_pos] = *str;
+		fp->buffer_pos ++, str++;
+	}
 	return 0;
 }
--- src/stdlib/puts.c
+++ src/stdlib/puts.c
@@ -4,6 +4,7 @@ int puts(const char *m) {
 	int go;
 	go = fputs(m   , stdout);
 	go = fputs("\n", stdout);
+	fflush(stdout);
 	return go;
 }
 
