28ee5c5b3190db3fda5876f5c0df079474bf8eb1
Dale Weiler <killfieldengine@gmail.com>
2012-01-21 16:02:19 -0500

    Removed file not required.

--- foo.c
+++
@@ -1,8 +0,0 @@
-extern void *_end;
-#define true (int)(!*((char*)&_end))
-#define false !true
-main() {
-	int x = true, false;
-	printf("x is %s\n",(x)?"true":"false");
-	printf("y is %s\n",(y)?"true":"false");
-}
