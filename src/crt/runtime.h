8b3c7c2e4dcc0d8720d9bf3bce1ef850c56e5909
Dale Weiler <killfieldengine@gmail.com>
2012-01-23 03:00:00 -0500

    use <sys/syscalls.h> for better portabilty. Remove brk syscall -- not required this is deprecated anyways even in linux.

--- src/crt/runtime.h
+++ src/crt/runtime.h
@@ -23,7 +23,7 @@
 #ifndef CCAPRICE_CRT_HDR
 #define CCAPRICE_CRT_HDR
 #include "inc/ccaprice.h"
-#include <syscall.h>
+#include <sys/syscall.h>
 
 typedef int32_t pid_t;
 
@@ -32,7 +32,6 @@ CCAPRICE_EXPORT ssize_t read  (int, void      *, size_t);
 CCAPRICE_EXPORT int     open  (const char     *, int);
 CCAPRICE_EXPORT int     close (int);
 CCAPRICE_EXPORT int     kill  (pid_t, int);
-CCAPRICE_EXPORT int     brk   (int);
 CCAPRICE_EXPORT void    _exit (int);
 CCAPRICE_EXPORT pid_t   getpid();
 
