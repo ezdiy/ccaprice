b8e135d204bb824fe00f1423685f1b98a9aa5464
Dale Weiler <killfieldengine@gmail.com>
2012-01-24 14:17:45 -0500

    Implemented standard posix <sysexit.h>

---
+++ inc/posix/sysexit.h
@@ -0,0 +1,46 @@
+/*
+ * Copyright (C) 2012 
+ * 	Dale Weiler
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+#ifndef CCAPRICE_POSIX_SYSEXIT_HDR
+#define CCAPRICE_POSIX_SYSEXIT_HDR
+
+#define EX__BASE	    64
+#define EX__MAX		    78
+
+#define EX_OK		    0
+#define EX_USAGE	    64
+#define EX_DATAERR	    65
+#define EX_NOINPUT	    66
+#define EX_NOUSER	    67
+#define EX_NOHOST	    68
+#define EX_UNAVAILABLE	69
+#define EX_SOFTWARE    	70
+#define EX_OSERR	    71
+#define EX_OSFILE	    72
+#define EX_CANTCREAT	73
+#define EX_IOERR	    74
+#define EX_TEMPFAIL	    75
+#define EX_PROTOCOL	    76
+#define EX_NOPERM	    77
+#define EX_CONFIG	    78
+
+#endif
