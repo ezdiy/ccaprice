f426ba9f32db4f4ad2583cb932a4d557b8516d0c
Dale Weiler <killfieldengine@gmail.com>
2012-07-07 13:47:36 -0400

    Fix indentation

--- inc/iso646.h
+++ inc/iso646.h
@@ -29,15 +29,15 @@
  * run it through a preprocessor.
  */
 #if !defined(__cplusplus)
-# define and     &&
-# define and_eq  &=
-# define bitand  &
-# define bitor   |
-# define compl   ~
-# define not     !=
-# define or      ||
-# define or_eq   |=
-# define xor     ^
-# define xor_eq  ^=
+#   define and     &&
+#   define and_eq  &=
+#   define bitand  &
+#   define bitor   |
+#   define compl   ~
+#   define not     !=
+#   define or      ||
+#   define or_eq   |=
+#   define xor     ^
+#   define xor_eq  ^=
 #endif
 #endif
